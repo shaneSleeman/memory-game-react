e70e09a52be94ced2bc7b9af571984cf504119a5
shaneSleeman <shanesleeman1@gmail.com>
2022-06-18 10:05:46 +1000

    Intro text

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import Card from './components/Card';
 
 function App() {
   return (
-    <div>
+    <div className="content">
+      <h1>Memory Game</h1>
+      <h2>How to Play</h2>
+      <p>Select as many unique cards in a row as possible.</p>
+      <p>If you select a card that you have already selected, the score will reset.</p>
+      <p>Let's begin!</p>
       <Card name="Item1" image="Test" />
     </div>
   );
